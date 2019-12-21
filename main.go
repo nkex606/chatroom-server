e391167ba4ff2fa9c818b2481d1c439727041717
John Chang <nkex606@gmail.com>
2019-12-22 00:15:22 +0800

    add healthz route

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -17,6 +18,14 @@ func main() {
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		ws.Hanlder(s, w, r)
 	})
+
+	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		healthz(w, r)
+	})
 	log.Printf("Start listening on port %s...", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+func healthz(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "ok")
 }
